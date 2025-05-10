a1128149a67099086924bbf2d6481d58a67b3574
Carlos Sechi <carlos@sechi.dev>
2025-05-10 04:58:40 +0000

    feat: add session create action

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+module API
+  class SessionsController < API::ApplicationController
+    allow_unauthenticated_access only: %i[create]
+
+    skip_before_action :authenticate_user!, only: %i[create]
+
+    rate_limit to: 10, within: 3.minutes, only: :create, with: -> { render_too_many_requests }
+
+    # @route POST /api/sessions (api_sessions)
+    def create
+      if (user = User.authenticate_by(params.permit(:email, :password)))
+        token = JsonWebToken.encode(build_token_payload(user))
+        render(json: { token: token }, status: :ok)
+      else
+        render(json: { error: 'Wrong email or password.' }, status: :unauthorized)
+      end
+    end
+
+    private
+
+    def build_token_payload(user)
+      { id: user.id, email: user.email }
+    end
+  end
+end
